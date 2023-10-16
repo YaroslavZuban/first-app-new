4337d7d27c2f4cede03788aa51696078e5eb7791
YaroslavZuban <72079338+YaroslavZuban@users.noreply.github.com>
2023-10-16 09:24:06 +0700

    Init

---
+++ src/main/java/com/eltex/Main.java
@@ -0,0 +1,29 @@
+package com.eltex;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final var scanner = new Scanner(System.in);
+
+        System.out.println("Введите сумму покупок за прошлый год");
+
+        final var clientYearlyPurchases = scanner.nextInt();
+        final var discount = 0.02;
+        final var discountStart = 3_000;
+
+        final double totalDiscount;
+
+        if (clientYearlyPurchases >= discountStart) {
+            totalDiscount = clientYearlyPurchases * discount;
+        } else {
+            totalDiscount = 0;
+        }
+
+        if (totalDiscount > 0) {
+            System.out.printf("Попробуйте нашу новую подписку и сэкономьте 2%%, ваша экономия составит %s ₽%n", totalDiscount);
+        } else {
+            System.out.println("За прошлый год вы бы сэкономили с подпиской 0 ₽");
+        }
+    }
+}
